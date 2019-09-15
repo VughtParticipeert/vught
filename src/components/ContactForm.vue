3a3d7263a98d3287b2eca07e7fef1ca18e03b15b
niyorn <c.niyornram@hotmail.com>
2019-09-15 14:27:00 +0200

    test Netlify Form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -5,6 +5,20 @@
         </g-link>
         <section class="content">
             <p>mail: <a :href="`mailto:${email}`">{{email}}</a></p>
+            <form name="contact" data-netlify="true">
+                <p>
+                    <label>Your Name: <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Your Email: <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <label>Message: <textarea name="message"></textarea></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
         </section>
     </article>
 </template>
