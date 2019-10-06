c9e1e746af3a25f16749a90f62a5bfcc114ea3b2
niyorn <c.niyornram@hotmail.com>
2019-10-06 18:00:18 +0200

    add: Mailchimp

--- src/components/Newsletter.vue
+++ src/components/Newsletter.vue
@@ -1,80 +1,104 @@
 <template>
-    <aside class="newsletter-container">
-        <h1 class="tittle">Nieuwsbrief</h1>
+	<aside class="newsletter-container">
+		<h1 class="tittle">Nieuwsbrief</h1>
 
-        <form @submit.prevent class="newsletter-form">
-            <label for="">
-                <p class="description">Meld je in voor onze online nieuwsbrief en blijf op de hoogte van de laatste nieuws en ontwikkelingen.</p>
-                <input class="email-input" type="email" required placeholder="jouw@email.nl">
-            </label>
+		<form
+			class="newsletter-form"
+			action="https://helmer.us20.list-manage.com/subscribe/post?u=ab39ac0446804123465ef85ab&amp;id=d6606b222b"
+			method="post"
+			id="mc-embedded-subscribe-form"
+			name="mc-embedded-subscribe-form"
+		>
+			<p
+				class="description"
+			>Meld je in voor onze online nieuwsbrief en blijf op de hoogte van de laatste nieuws en ontwikkelingen.</p>
+			<label>
+				<span>Email adres</span>
+				<input type="email" value name="EMAIL" class="email-input" placeholder="jouw@email.nl" required />
+			</label>
 
-            <button class="subscribe-button">Aanmelden</button>
-        </form>
-    </aside> 
+			<div id="mce-responses" class="clear">
+				<div class="response" id="mce-error-response" style="display:none"></div>
+				<div class="response" id="mce-success-response" style="display:none"></div>
+			</div>
+
+			<input type="submit" value="Aanmelden" name="subscribe" class="subscribe-button" />
+		</form>
+	</aside>
 </template>
 
 <script>
 export default {
-    name: "Newsletter"
-}
+	name: "Newsletter"
+};
 </script>
 
 <style lang="scss" scoped>
-    .newsletter-container {
-        background-color: var(--accent-color-two);
-        width: 20rem;
-        margin-top: 12.85rem;
-        padding: 2rem;
-        position: sticky;
-        top: 5rem;
-        border-radius: var(--small-radius);
+.newsletter-container {
+	background-color: var(--accent-color-two);
+	width: 20rem;
+	margin-top: 12.85rem;
+	padding: 2rem;
+	position: sticky;
+	top: 5rem;
+	border-radius: var(--small-radius);
 
-        .tittle {
-            font-size: 2em;
-            color: var(--primary-color);
-            color: white;
-        }
+	.tittle {
+		font-size: 2em;
+		color: var(--primary-color);
+		color: white;
+	}
 
-        .newsletter-form {
-            margin-top: 2rem;
+	.newsletter-form {
+		margin-top: 2rem;
 
-            .description {
-                color: white;
-            }
+		.description {
+			color: white;
+		}
 
-            .email-input {
-                border: none;
-                border-radius: var(--small-radius);
-                padding: 1rem;
-                margin-top: 2rem;
-                width: 100%;
-                background-color: rgba(0, 0, 0, 0.04);
-                font-size: 1.4em;
-                color: white;
+		label {
+			display: inline-block;
+			margin-top: 2rem;
 
-                &::placeholder {
-                    color: rgb(230, 230, 230);
-                }
-            }
+			span {
+				color: rgb(240, 240, 240);
+			}
+		}
 
-            .subscribe-button {
-                margin-top: 2rem;
-                padding: 1rem;
-                border: none;
-                border-radius: var(--small-radius);
-                font-weight: 500;
-                width: 100%;
-                font-size: 1.1em;
-                background-color: var(--secondary-color);
-                color: white;
-                transition: all 0.4s ease-out;
-                box-shadow: var(--material-shadow-one);
+		.email-input {
+			border: none;
+			border-radius: var(--small-radius);
+			padding: 1rem;
+			margin-top: 0.4rem;
+			width: 100%;
+			background-color: rgba(0, 0, 0, 0.04);
+			font-size: 1.4em;
+			color: white;
 
-                &:hover {
-                    cursor: pointer;
-                    box-shadow: var(--material-shadow-hover);
-                }
-            }
-        }
-    }
+			&::placeholder {
+				color: rgb(230, 230, 230);
+			}
+		}
+
+		.subscribe-button {
+			margin-top: 2rem;
+			padding: 1rem;
+			border: none;
+			border-radius: var(--small-radius);
+			font-weight: 500;
+			width: 100%;
+			font-size: 1.1em;
+			background-color: var(--secondary-color);
+			color: white;
+			transition: all 0.4s ease-out;
+			box-shadow: var(--material-shadow-one);
+			text-align: center;
+
+			&:hover {
+				cursor: pointer;
+				box-shadow: var(--material-shadow-hover);
+			}
+		}
+	}
+}
 </style>
